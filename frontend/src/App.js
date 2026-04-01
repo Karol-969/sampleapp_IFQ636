8b5961ae8dff7a67ca835b2ba0737317599be293
Karol-969 <karolbhandari78@gmail.com>
2026-04-02 01:33:45 +1000

    update app routes for nutrition plans

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,30 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Dashboard from './pages/Dashboard';
+import Plans from './pages/Plans';
+import CreatePlan from './pages/CreatePlan';
+import EditPlan from './pages/EditPlan';
+import PlanDetail from './pages/PlanDetail';
+import PlanMeals from './pages/PlanMeals';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/plans/new" element={<CreatePlan />} />
+          <Route path="/plans/:id" element={<PlanDetail />} />
+          <Route path="/plans/:id/edit" element={<EditPlan />} />
+          <Route path="/plans/:id/meals" element={<PlanMeals />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
